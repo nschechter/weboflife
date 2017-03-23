4c629788c80b9d787cc746129222fb8f320bdd9f
Noah Schechter <noahschechter7@gmail.com>
2017-03-22 20:01:25 -0400

    fixed bugs and updated to version 0.2

--- content.js
+++ content.js
@@ -3,17 +3,20 @@
  */
  const CELL_WIDTH = 50;
  const CELL_HEIGHT = 50;
+ const TICK_RATE = 50; // in ms
 
  var cells = [];
  var canvas;
+ var statusCanvas = document.createElement("canvas");
+ var status = "Preparing...";
 
+ // Cell class
  class Cell {
  	constructor(x, y) {
  		this.x = x;
  		this.y = y;
  		this.alive = false;
  		this.neighbors = [];
- 		// maybe canvas instead
  		this.imageData = null;
  	}
 
@@ -23,24 +26,24 @@
 
  	drawCell(canvas) {
  		let context = canvas.getContext("2d");
-		context.moveTo(this.getCoordinate().x, this.getCoordinate().y);
-		context.lineTo(this.getCoordinate().x + 50, this.getCoordinate().y);
-		context.moveTo(this.getCoordinate().x, this.getCoordinate().y);
-		context.lineTo(this.getCoordinate().x, this.getCoordinate().y + 50);
+ 		context.moveTo(this.getCoordinate().x, this.getCoordinate().y);
+ 		context.lineTo(this.getCoordinate().x + 50, this.getCoordinate().y);
+ 		context.moveTo(this.getCoordinate().x, this.getCoordinate().y);
+ 		context.lineTo(this.getCoordinate().x, this.getCoordinate().y + 50);
 
-		context.strokeStyle = "black";
-		context.stroke();
-	}
+ 		context.strokeStyle = "black";
+ 		context.stroke();
+ 	}
 
-	drawState(canvas) {
-		let context = canvas.getContext("2d");
-		context.fillText(this.isAlive(), this.getCoordinate().x, this.getCoordinate().y);
-		context.stroke();
-	}
+ 	drawState(canvas) {
+ 		let context = canvas.getContext("2d");
+ 		context.fillText(this.isAlive(), this.getCoordinate().x, this.getCoordinate().y);
+ 		context.stroke();
+ 	}
 
-	setCanvasAndState(canvas) {
-		let context = canvas.getContext("2d");
-		let imageData = context.getImageData(this.getCoordinate().x, this.getCoordinate().y, 50, 50);
+ 	setCanvasAndState(canvas) {
+ 		let context = canvas.getContext("2d");
+ 		let imageData = context.getImageData(this.getCoordinate().x, this.getCoordinate().y, 50, 50);
 		//let aliveArray = [];
 		let aliveArraySample = [];
 		for (let i = 0; i < (CELL_HEIGHT * CELL_WIDTH); i ++) {
@@ -73,6 +76,18 @@
 	}
 }
 
+// WIP
+const updateStatus = (text) => {
+	status = text;
+	let context = statusCanvas.getContext("2d");
+	statusCanvas.width = 100;
+    statusCanvas.height = 100;
+	context.clearRect(100, 100, 100, 100);
+	context.font = "30px Arial";
+	context.fillText(status, 100, 100);
+}
+
+// needs refactoring
 const getCellAtCoordinate = (x, y) => {
 	let tempCell = undefined;
 	for (let cell of cells) {
@@ -81,16 +96,10 @@ const getCellAtCoordinate = (x, y) => {
 			break;
 		}
 	}
-	// cells.forEach((cell) => {
-	// 	if (cell.getCoordinate().x === x && cell.getCoordinate().y === y) {
-	// 		tempCell = cell;
-	// 		break;
-	// 	}
-	// })
 	return tempCell;
 }
 
-// to do more with accuracy
+// needs refactoring
 const getMajorityColor = (arr) => {
 	let b = 0;
 	let w = 0;
@@ -103,6 +112,7 @@ const getMajorityColor = (arr) => {
 	return b > w ? "black" : "white"
 }
 
+// destroys a cell by setting alive to false and setting opacity to 0.
 const destroyCell = (cell) => {
 	cell.alive = false;
 	let data = cell.imageData;
@@ -110,6 +120,7 @@ const destroyCell = (cell) => {
 	cell.setImageData(data);
 }
 
+// assign random image data from a neighbor to the cell specified
 const getRandomImageDataFromNeighbor = (cell) => {
 	cell.alive = true;
 	neighbors = cell.getNeighbors();
@@ -117,6 +128,7 @@ const getRandomImageDataFromNeighbor = (cell) => {
 	cell.setImageData(randomNeighbor.imageData);
 }
 
+// actual conways game of life rules
 const calculateCellMove = (cell) => {
 	let neighbors = cell.getNeighbors();
 	if (cell.isAlive()) {
@@ -130,19 +142,24 @@ const calculateCellMove = (cell) => {
 	}
 }
 
+// run game ticks
 const runGame = () => {
 	setInterval(() => {
 		step();
+		status = "Running";
+		updateStatus(status);
 		console.log("stepping");
-	}, 50);
+	}, TICK_RATE);
 }
 
+// actual tick step
 const step = () => {
 	cells.forEach((cell) => {
 		calculateCellMove(cell);
 	});
 }
 
+// create canvas and start the game
 const createSS = (event) => {
 	console.log("LOADED");
 	html2canvas(document.body, {
@@ -156,19 +173,27 @@ const createSS = (event) => {
 
 // creates grid
 const createGrid = (canvas) => {
-		//grid width and height
-		var bw = canvas.width;
-		var bh = canvas.height;
-
-		//padding around grid (set to 0 for now)
-		var p = 0;
-		//size of canvas
-		var cw = bw + (p*2) + 1;
-		var ch = bh + (p*2) + 1;
-
-		var context = canvas.getContext("2d");
-		//(bw / 50) * (bh / 50)
 		console.log("...drawing grid");
+		updateStatus("Drawing Grid...");
+		let bw = canvas.width;
+		let bh = canvas.height;
+
+		let context = canvas.getContext("2d");
+
+		for (let x = 0; x <= bw; x += 50) {
+			context.moveTo(x, 0);
+			context.lineTo(x, bh);
+		}
+
+		for (let y = 0; y <= bh; y += 50) {
+			context.moveTo(0, y);
+			context.lineTo(bw, y);
+		}
+
+		context.stroke();
+
+		console.log("...done drawing grid");
+		//updateStatus("Creating Cells...");
 
 		for (let x = 0; x < bw; x += CELL_WIDTH) {
 			for (let y = 0; y < bh; y += CELL_HEIGHT) {
@@ -179,6 +204,7 @@ const createGrid = (canvas) => {
 		}
 
 		console.log("...created cells");
+		//updateStatus("Creating Neighbors...")
 
 		// add surrounding neighbors
 		for (let x = 0; x < bw; x += CELL_WIDTH) {
@@ -193,15 +219,21 @@ const createGrid = (canvas) => {
 				cell.addNeighbor(getCellAtCoordinate(x+50, y-50))
 				cell.addNeighbor(getCellAtCoordinate(x-50, y+50))
 				cell.setCanvasAndState(canvas);
-				cell.drawState(canvas);
+				//cell.drawState(canvas);
 			}
 		}
+
 		console.log("...added neighbors to cells");
+
+		// wipe body html and replace it with the canvas
 		document.body.innerHTML = "";
 		document.body.appendChild(canvas);
-	};
+ };
 
-// returns image data for the entire canvas
+/*                                                                *
+ * ====================Possible Greyscaling?===================== *
+ *                             (WIP)                              */
+
 const getImageData = (canvas) => {
 	var context = canvas.getContext("2d");
 	var width = canvas.width;
@@ -210,14 +242,14 @@ const getImageData = (canvas) => {
 	return imageData;
 };
 
-    const greyScale = (canvas) => {
-    	var context = canvas.getContext("2d");
-    	var imageData = getImageData(canvas);
-    	var data = imageData.data;
+const greyScale = (canvas) => {
+	var context = canvas.getContext("2d");
+	var imageData = getImageData(canvas);
+	var data = imageData.data;
 
-		console.log("...greyscaling");
-    	for(var i = 0; i < data.length; i += 4) {
-    		var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
+	console.log("...greyscaling");
+	for(var i = 0; i < data.length; i += 4) {
+		var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
           // red
           if (data[i] >= 225) {
           	data[i] = 255;
@@ -250,4 +282,5 @@ const getImageData = (canvas) => {
         return canvas;
     };
 
-    window.addEventListener ("load", createSS, false);
+// run function on execution of script
+createSS();
